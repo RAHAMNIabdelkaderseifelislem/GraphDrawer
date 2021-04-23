57551c114891ba02fef5d7da7ae84bffb267d208
RAHMANI Abd El Kader Seif El Islem <77025510+RAHAMNIabdelkaderseifelislem@users.noreply.github.com>
2021-04-23 14:23:53 +0100

    Update mathGraphs.py

--- mathGraphs.py
+++ mathGraphs.py
@@ -8,12 +8,23 @@ def draw(x,y):
     plt.title('THE GRAPH')
     plt.show()
 
+def inter(x,z,t):
+
+    a = []
+    b = []
+    while(x<z):
+        a.append(x)
+        y = eval(t)
+        b.append(y)
+        x = x + 0.1
+    
+    a.append(x)
+    b.append(eval(t))
+
+    draw(a,b)
+
+
 x0 = int(input("Enter first x : "))
 x1 = int(input("Enter last x : "))
 equation = input("your equation y = f(x) :  y = ")
-y = []
-z = []
-for x in range(x0,x1,1):
-    y.append(eval(equation))
-    z.append(x)
-draw(z,y)
+inter(x0,x1,equation)
